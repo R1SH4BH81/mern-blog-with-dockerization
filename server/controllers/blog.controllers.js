95f595741c0ad0d845e2272ca93e85bc14a7752a
Rishabh Mishra <rishabhmishra.81e@gmail.com>
2025-08-13 20:49:52 +0530

    mw + controllers completed

---
+++ server/controllers/blog.controllers.js
@@ -0,0 +1,97 @@
+const Blog = require("../models/blog.model");
+
+const createBlog = async (req, res) => {
+  try {
+    const { title, content } = req.body;
+    const blog = new Blog({ title, content, author: req.user.id });
+    await blog.save();
+    res.status(200).json;
+  } catch (err) {
+    return res.status(500).json({ message: "Server Error" });
+  }
+};
+
+const getAllBlogs = async (req, res) => {
+  try {
+    const blogs = await Blog.find()
+      .populate("author", "name email")
+      .sort({ createdAt: -1 });
+    res.status(200).json(blogs);
+  } catch (err) {
+    return res.status(500).json({ message: "Server Error" });
+  }
+};
+
+const getLatestBlogs = async (req, res) => {
+  try {
+    const blogs = await Blog.find()
+      .populate("author", "name email")
+      .sort({ createdAt: -1 })
+      .sort(6);
+    res.status(200).json(blogs);
+  } catch (err) {
+    return res.status(500).json({ message: "Server Error" });
+  }
+};
+
+const getAuthorBlogs = async (req, res) => {
+  try {
+    const blogs = await Blog.find({ author: req.user.id })
+      .populate("author", "name email")
+      .sort({ createdAt: -1 })
+      .sort(6);
+    res.status(200).json(blogs);
+  } catch (err) {
+    return res.status(500).json({ message: "Server Error" });
+  }
+};
+
+const updateBlogs = async (req, res) => {
+  try {
+    const blog = await Blog.findById(req.params.id);
+    if (!blog) {
+      return res.status(404).json({ message: "blog not found" });
+    }
+    if (blog.author.toString() == req.user.id) {
+      return req.status(400).json({ message: "not authorize to edit" });
+    }
+
+    blog.title = req.body.title || blog.title;
+    blog.content = req.body.content || blog.content;
+    blog.image = req.body.image || blog.image;
+
+    await blog.save();
+
+    res.status(201).json({ message: "Blog updated successfully" });
+  } catch (err) {
+    return res.status(500).json({ message: "Server Error" });
+  }
+};
+
+const deleteBlog = async (req, res) => {
+  try {
+    const blog = await Blog.findById(req.params.id);
+
+    if (!blog) return res.status(404).json({ message: "Blog not found" });
+
+    if (blog.author.toString() !== req.user.id) {
+      return res
+        .status(403)
+        .json({ message: "Not authorized to delete this blog" });
+    }
+
+    await blog.deleteOne();
+    res.json({ message: "Blog deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+module.exports = {
+  deleteBlog,
+  updateBlogs,
+  getAllBlogs,
+  getAuthorBlogs,
+  getLatestBlogs,
+  createBlog,
+};
